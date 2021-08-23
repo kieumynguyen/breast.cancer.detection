30470d01e3f5baedc15193cecfac9e6e68e9d828
kieumynguyen <kieumynguyen700@gmail.com>
2021-08-23 15:13:17 +0700

    add refactor

---
+++ scr/model.py
@@ -0,0 +1,12 @@
+from keras.models import Sequential #  one model in  keras
+from keras.layers import Dense # dense is 1 layer
+
+class BreastcancerModel:
+    def __init__(self):
+        self.classifier = Sequential() # Initialising the ANN creates a blank model
+        self.classifier.add(Dense(units = 16, activation = 'relu', input_dim = 30)) # create layer; units =16 (nodes) is input dim= 30
+        self.classifier.add(Dense(units = 8, activation = 'relu'))# 70% use relu, keep the value and remove the negative value; hidden layer
+        self.classifier.add(Dense(units = 6, activation = 'relu'))
+        self.classifier.add(Dense(units = 1, activation = 'sigmoid')) # To break the result, we also classify 2 classes ; output layer
+    def load_model(self):
+        return self.classifier
---
+++ train.py
@@ -0,0 +1,16 @@
+from scr.model import BreastcancerModel
+import pandas as pd
+#load data
+X_train = pd.read_csv("data/xtrain.csv", header=None)
+Y_train = pd.read_csv("data/ytrain.csv", header=None)
+X_test = pd.read_csv("data/xtest.csv", header=None)
+Y_test = pd.read_csv("data/ytest.csv", header=None)
+#load model
+classifier = BreastcancerModel().load_model()
+classifier.compile(optimizer = 'rmsprop', loss = 'binary_crossentropy', metrics = ["accuracy"])
+#train
+history=classifier.fit(X_train, Y_train, batch_size = 1, epochs = 20 )
+#validate (predict on test set)
+#T=0.5
+#Y_pred = classifier.predict(X_test)
+#Y_pred = [ 1 if y=T   else 0 for y in Y_pred ]
